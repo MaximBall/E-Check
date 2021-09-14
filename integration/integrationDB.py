df2685f79c64ae11942f20b18531cbae7c728782
MaximBall <maks1111bm@gmail.com>
2021-09-14 20:34:08 +0300

    connect to MongoDB

---
+++ integration/integrationDB.py
@@ -0,0 +1,32 @@
+from .secret import PASSWORD, DB
+
+def get_database():
+
+    # Provide the mongodb atlas url to connect python to mongodb using pymongo
+    CONNECTION_STRING = f"mongodb+srv://mks:{PASSWORD}@cluster0.qvwnv.mongodb.net/{DB}?retryWrites=true&w=majority"
+
+    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+    from pymongo import MongoClient
+    client = MongoClient(CONNECTION_STRING)
+
+    # Create the database for our example (we will use the same database throughout the tutorial
+    return client[DB]
+    
+# This is added so that many files can reuse the function get_database()
+if __name__ == "__main__":    
+    
+    # Get the database
+    print("Try connect")
+    dbname = get_database()
+    print("Connect")
+
+    collection_name = dbname["user_1_items"]
+
+
+    item_2 = {
+        "item_name" : "NEW_OKI",
+        "price" : 342,
+        "category" : "kitchen appliance"
+    }
+
+    collection_name.insert_one(item_2)
